46af0fed6a0ad91ccec9b72899a9b7d44b04ee21
sharma-akshat <72413891+sharma-akshat@users.noreply.github.com>
2021-07-29 11:43:59 +0530

    array reversed

--- reverseArray.java
+++ reverseArray.java
@@ -3,18 +3,18 @@
 import java.util.*;
 class reverseArray
 {
-public static void main(String gg[])
-{
-Scanner s=new Scanner(System.in);
-System.out.print("Enter size of array:");
-int n=s.nextInt();
-int x[]=new int[n];
-for(int i=0;i<n;i++)
-{
-System.out.print("Enter number:");
-x[i]=s.nextInt();
-}
-System.out.println("Reversed array");
-for(int j=n-1;j>=0;j--)System.out.println(x[j]);
-}
+     public static void main(String gg[])
+     {
+        Scanner s=new Scanner(System.in);
+        System.out.print("Enter size of array:");
+        int n=s.nextInt();
+        int x[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+          System.out.print("Enter number:");
+          x[i]=s.nextInt();
+        }
+        System.out.println("Reversed array");
+        for(int j=n-1;j>=0;j--)System.out.println(x[j]);
+     }
 }
